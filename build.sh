e6c21fa0f12d052acbf29541cb49dd279161061d
Roshnipriya <127740197+roshnipriyarameshkumar@users.noreply.github.com>
2025-04-06 14:09:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker build -t task_final .
 echo Hyyy
-docker login -u naveen11122004 -p naveen2004
-docker tag task_final naveen11122004/devops_final
-docker push naveen11122004/devops_final
+docker login -u roshnipriya17 -p roshni123
+docker tag task_final roshnipriya17/miniproject
+docker push roshnipriya17/miniproject
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
