3b753f1c3ed271059a7e39744928c6301e1c2def
Roshnipriya <127740197+roshnipriyarameshkumar@users.noreply.github.com>
2025-04-06 22:14:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ echo Hyyy
 docker login -u roshnipriya17 -p roshni123
 docker tag task_final roshnipriya17/miniproject
 docker push roshnipriya17/miniproject
+minikube delete
+minikube start
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
